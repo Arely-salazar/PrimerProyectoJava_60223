8e1a1633f83e260574a6922b1947de47d5a7d270
Arely <Jairo@LAPTOP-TE9AK6EP>
2021-02-27 18:54:28 -0600

    primer commit

---
+++ src/primerproyectojava_60223/PrimerProyectoJava_60223.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyectojava_60223;
+
+/**
+ *
+ * @author Jairo
+ */
+public class PrimerProyectoJava_60223 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          int ejercicio = 4;
+        
+        char letra = 85  ;
+        
+        boolean alto = true; 
+        
+        System.out.println("Ejercicio Número: " + ejercicio);
+        System.out.println("La letra es: "+ letra);
+        System.out.println("Es alto: " + alto );
+    
+    }
+    
+}
